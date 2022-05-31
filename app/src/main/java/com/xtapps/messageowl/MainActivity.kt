b8cf1a3d572d2c09c0c70b32337dfc7673b0d7ae
Omasyo <cesare3000@outlook.com>
2022-05-31 08:21:45 +0100

    before the storm

--- app/src/main/java/com/xtapps/messageowl/MainActivity.kt
+++ app/src/main/java/com/xtapps/messageowl/MainActivity.kt
@@ -3,7 +3,9 @@ package com.xtapps.messageowl
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,12 +23,21 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+//        val options = NavOptions.Builder()
+//            .setLaunchSingleTop(true)
+//            .setEnterAnim(android.R.anim.fade_in)
+//            .setExitAnim(android.R.anim.fade_out)
+//            .setPopEnterAnim(android.R.anim.fade_in)
+//            .setPopExitAnim(android.R.anim.fade_out)
+//            .build()
+//        navView.setupWithNavController(navController)
         navView.setOnItemSelectedListener {
             binding.materialToolbar.title = it.title
             navController.navigate(it.itemId)
-            false
+            true
         }
     }
 
