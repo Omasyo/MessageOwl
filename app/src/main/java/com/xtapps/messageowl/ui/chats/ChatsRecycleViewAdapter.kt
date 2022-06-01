77531ac206d820426c26efd63e2ef8aa016b1278
Omasyo <cesare3000@outlook.com>
2022-06-01 22:42:55 +0100

    chat recycle viewer

---
+++ app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsRecycleViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.xtapps.messageowl.ui.chats
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.xtapps.messageowl.R
+
+class ChatsRecycleViewAdapter : RecyclerView.Adapter<ChatsRecycleViewAdapter.ViewHolder>() {
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val textView =view.findViewById<TextView>(R.id.text_home)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.chat_view, parent, false)
+        return ViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = "Chats $position"
+    }
+
+    override fun getItemCount(): Int {
+        return 100
+    }
+}
