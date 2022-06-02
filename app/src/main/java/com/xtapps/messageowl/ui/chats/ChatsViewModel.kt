f89584c23cdf15186bfc71bce06bdaff0c1628d4
Omasyo <cesare3000@outlook.com>
2022-06-02 17:09:22 +0100

    refactor

--- app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsViewModel.kt
+++ app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsViewModel.kt
@@ -1,13 +1,14 @@
 package com.xtapps.messageowl.ui.chats
 
+import androidx.core.util.rangeTo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ChatsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _chats = MutableLiveData<List<String>>().apply {
+        value = (1).rangeTo(100).toList().map { "Chat $it" ***REMOVED***
     ***REMOVED***
-    val text: LiveData<String> = _text
+    val chats: LiveData<List<String>> = _chats
 ***REMOVED***
