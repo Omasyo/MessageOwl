18f31a7c22d5601be97b40fb3f804f0312c20ba3
Omasyo <cesare3000@outlook.com>
2022-06-07 19:11:10 +0100

    country codes addition

--- app/src/main/java/com/xtapps/messageowl/VerifyFragment.kt
+++ app/src/main/java/com/xtapps/messageowl/VerifyFragment.kt
@@ -1,10 +1,12 @@
 package com.xtapps.messageowl
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.xtapps.messageowl.databinding.FragmentVerifyBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,8 @@ class VerifyFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var _binding: FragmentVerifyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +37,12 @@ class VerifyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verify, container, false)
+        _binding = FragmentVerifyBinding.inflate(inflater, container, false)
+        _binding.proceedButton.setOnClickListener {
+            findNavController().navigate(R.id.action_verifyFragment_to_completeProfileFragment)
+        }
+
+        return _binding.root
     }
 
     companion object {
