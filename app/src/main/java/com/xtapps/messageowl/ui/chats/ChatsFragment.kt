051250bd93fce593446580f5eaf9545107500f86
Omasyo <cesare3000@outlook.com>
2022-07-01 21:48:45 +0100

    database integration improved

--- app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsFragment.kt
@@ -1,6 +1,7 @@
 package com.xtapps.messageowl.ui.chats
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xtapps.messageowl.MessageOwlApplication
 import com.xtapps.messageowl.databinding.FragmentChatsBinding
+import com.xtapps.messageowl.ui.home.HomeFragmentDirections
 
 class ChatsFragment : Fragment() {
 
@@ -35,13 +38,23 @@ class ChatsFragment : Fragment() {
         _binding = FragmentChatsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val adapter = ChatsRecyclerViewAdapter { view, position ->
+            val action = HomeFragmentDirections.actionHomeFragmentToDirectMessageFragment("Chats Screen $position")
+            view.findNavController().navigate(action)
+        ***REMOVED***
 
         recyclerView = binding.chatsRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
 
+        Log.d("QWERr", "onCreateView: ${viewModel.allChats().asLiveData()***REMOVED***")
+
         viewModel.allChats().asLiveData().observe(viewLifecycleOwner) {
-            recyclerView.adapter = ChatsRecyclerViewAdapter(it)
+            it.let {
+                adapter.submitList(it)
+            ***REMOVED***
         ***REMOVED***
+
+        recyclerView.adapter = adapter
 
         return root
     ***REMOVED***
--- app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsRecyclerViewAdapter.kt
+++ app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsRecyclerViewAdapter.kt
@@ -1,37 +1,67 @@
 package com.xtapps.messageowl.ui.chats
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.xtapps.messageowl.R
 import com.xtapps.messageowl.database.ChatRoomDatabase
 import com.xtapps.messageowl.models.ChatRoom
 import com.xtapps.messageowl.ui.home.HomeFragmentDirections
 
-class ChatsRecyclerViewAdapter(private val test: List<ChatRoom>) : RecyclerView.Adapter<ChatsRecyclerViewAdapter.ViewHolder>() {
+class ChatsRecyclerViewAdapter(private val onClick: (View, position: Int) -> Unit) : RecyclerView.Adapter<ChatsRecyclerViewAdapter.ViewHolder>() {
+    private var test: List<ChatRoom> = listOf()
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.text_home)
         val cardView: CardView = view.findViewById(R.id.card_view)
     ***REMOVED***
 
+    fun submitList(list: List<ChatRoom>) {
+        Log.d("TAG", "submitList: submitting")
+        val result = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int {
+                return test.size
+            ***REMOVED***
+
+            override fun getNewListSize(): Int {
+                return list.size
+            ***REMOVED***
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return test[oldItemPosition] == list[newItemPosition]
+            ***REMOVED***
+
+            override fun areContentsTheSame(
+                oldItemPosition: Int,
+                newItemPosition: Int
+***REMOVED***: Boolean {
+                return test[oldItemPosition] == list[newItemPosition]
+            ***REMOVED***
+        ***REMOVED***)
+        test = list
+        result.dispatchUpdatesTo(this)
+    ***REMOVED***
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.chat_card_view, parent, false)
+        val viewHolder = ViewHolder(view)
+        viewHolder.bindingAdapterPosition
 
-        return ViewHolder(view)
+        return viewHolder
     ***REMOVED***
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.text = test[position].name
         holder.cardView.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToDirectMessageFragment("Chats Screen $position")
-            it.findNavController().navigate(action)
+            onClick(it, position)
         ***REMOVED***
     ***REMOVED***
 
