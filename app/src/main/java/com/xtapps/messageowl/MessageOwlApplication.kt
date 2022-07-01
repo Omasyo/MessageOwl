bf62046eb95e9f482a385902b6584d5c3a3949cc
Omasyo <cesare3000@outlook.com>
2022-07-01 21:48:45 +0100

    database integration improved

--- app/src/main/java/com/xtapps/messageowl/MessageOwlApplication.kt
+++ app/src/main/java/com/xtapps/messageowl/MessageOwlApplication.kt
@@ -1,8 +1,12 @@
 package com.xtapps.messageowl
 
 import android.app.Application
+import android.util.Log
 import com.xtapps.messageowl.database.ChatRoomDatabase
 
 class MessageOwlApplication: Application() {
-    val chatRoomDatabase: ChatRoomDatabase by lazy { ChatRoomDatabase.getDatabase(this) }
+    val chatRoomDatabase: ChatRoomDatabase by lazy {
+        Log.d("Datassar", "lazying about")
+        ChatRoomDatabase.getDatabase(this)
+    }
 }
--- app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsRecyclerViewAdapter.kt
+++ app/src/main/java/com/xtapps/messageowl/ui/chats/ChatsRecyclerViewAdapter.kt
@@ -1,37 +1,67 @@
 package com.xtapps.messageowl.ui.chats
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.xtapps.messageowl.R
 import com.xtapps.messageowl.database.ChatRoomDatabase
 import com.xtapps.messageowl.models.ChatRoom
 import com.xtapps.messageowl.ui.home.HomeFragmentDirections
 
-class ChatsRecyclerViewAdapter(private val test: List<ChatRoom>) : RecyclerView.Adapter<ChatsRecyclerViewAdapter.ViewHolder>() {
+class ChatsRecyclerViewAdapter(private val onClick: (View, position: Int) -> Unit) : RecyclerView.Adapter<ChatsRecyclerViewAdapter.ViewHolder>() {
+    private var test: List<ChatRoom> = listOf()
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.text_home)
         val cardView: CardView = view.findViewById(R.id.card_view)
     }
 
+    fun submitList(list: List<ChatRoom>) {
+        Log.d("TAG", "submitList: submitting")
+        val result = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int {
+                return test.size
+            }
+
+            override fun getNewListSize(): Int {
+                return list.size
+            }
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return test[oldItemPosition] == list[newItemPosition]
+            }
+
+            override fun areContentsTheSame(
+                oldItemPosition: Int,
+                newItemPosition: Int
+            ): Boolean {
+                return test[oldItemPosition] == list[newItemPosition]
+            }
+        })
+        test = list
+        result.dispatchUpdatesTo(this)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.chat_card_view, parent, false)
+        val viewHolder = ViewHolder(view)
+        viewHolder.bindingAdapterPosition
 
-        return ViewHolder(view)
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.text = test[position].name
         holder.cardView.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToDirectMessageFragment("Chats Screen $position")
-            it.findNavController().navigate(action)
+            onClick(it, position)
         }
     }
 
